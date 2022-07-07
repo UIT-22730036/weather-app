b06724809fe9ff7dfb1a01ce8fc0fb885dafcf98
S3CORP\khang.huynh <ankhang261096@gmail.com>
2022-07-07 11:57:16 +0700

    .

--- src/datas/chartData.js
+++ src/datas/chartData.js
@@ -1,19 +1,3 @@
 export const yDataMorning = [0, 2, 0, -2, 0, 2, 0, -2, 0, 2, 0]
 
-export const formatTime = time => {
-  const fmTime = time % 24
-  const decimal = fmTime % 1
-  const hr = Math.floor(fmTime)
-  const min = Math.floor(60 * decimal)
-  if (hr > 12) {
-    return `${hr - 12}:${min > 10 ? '' : '0'}${min} pm`
-  } else if (hr === 0) {
-    return `${12}:${min > 10 ? '' : '0'}${min} pm`
-  }
-  return `${hr}:${min > 10 ? '' : '0'}${min} am`
-}
 
-export const convertScrollToTime = scrollPercentage => {
-  // scrollPercentage * full time range + 6
-  return scrollPercentage * 60 + 6
-}
